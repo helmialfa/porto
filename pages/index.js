8be635f6e790b3e21a9ff053eba19c08d1002283
helmialfa <helmi.alfaiz@students.amikom.ac.id>
2023-12-20 19:39:02 +0700

    rapiin doang

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,8 @@ export default function Home() {
                 <button
                   className="bg-gradient-to-r from-pink-500 text- to-rose-400 text-white px-4 py-2 border-none rounded-md ml-8 hover:scale-110 hover:animate-pulse transition-all ease-in-out">
                   Ini Tombol
-                </button></a>
+                </button>
+                </a>
               </li>
             </ul>
           </nav>
